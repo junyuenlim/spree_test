7343fe3664b7dbd753b7ef9bebddbdb20c66471c
Jun Lim <junyuen@gmail.com>
2014-07-11 04:22:10 +0800

    Initial Deploy to Heroku

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,6 +9,10 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
+  config.use_s3 = true
+  config.s3_bucket = ENV['AWS_BUCKET']
+  config.s3_access_key = ENV['AWS_ACCESS_KEY_ID']
+  config.s3_secret = ENV['AWS_SECRET_ACCESS_KEY']
 end
 
 Spree.user_class = "Spree::User"
