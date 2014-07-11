5a81dfda4fb7074d3d0bb614f081771eb1de9190
Jun Lim <junyuen@gmail.com>
2014-07-11 15:27:10 +0800

    Deploy Heroku – Fix Paperclip Initializer

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,10 +9,12 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
-  config.use_s3 = true
-  config.s3_bucket = ENV['AWS_BUCKET']
-  config.s3_access_key = ENV['AWS_ACCESS_KEY_ID']
-  config.s3_secret = ENV['AWS_SECRET_ACCESS_KEY']
+  Paperclip::Attachment.default_options[:storage] = :s3
+  Paperclip::Attachment.default_options[:s3_credentials] = {
+  									:bucket => ENV['S3_BUCKET_NAME'],
+									:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    								:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+								}
 end
 
 Spree.user_class = "Spree::User"
